3ba978bb81ab3a2f37c567b5afeb23eb90db7b7a
phi <shusan.california@gmail.com>
2021-04-12 08:16:29 -0700

    test without building

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 const path = require('path');
 
-module.exports = [{
-  name: 'production',
+module.exports = {
   mode: 'none',
   entry: './src/index.js',
   output: {
@@ -26,28 +25,4 @@ module.exports = [{
       inline: 'main',
     })
   ],
-}, {
-  name: 'test',
-  mode: 'none',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, './test'),
-    filename: 'main.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    }),
-    new ScriptExtHtmlWebpackPlugin({
-      inline: 'main',
-    })
-  ],
-}];
+};
