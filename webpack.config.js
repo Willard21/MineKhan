a066ed3045a160937a23d68dc1e6fe120b0d7186
Willard21 <zyx870@yahoo.com>
2021-06-07 20:21:55 -0500

    Make it auto-deply on willard.fun

--- webpack.config.js
+++ webpack.config.js
@@ -6,27 +6,33 @@ const fs = require('fs');
 // hacky way to remove refference to the files but hey, it works
 const template = fs.readFileSync('./src/index.html', 'utf8').replace('<link rel="stylesheet" href="index.css">', '').replace('<script type="module" src="index-test.js"></script>', '');
 
+// I know it's a bad idea, but I don't really care.
+let output = "./dist"
+if (__dirname.includes("willard/server/dev")) {
+	output = "../../public/minekhan"
+}
+
 module.exports = {
-  mode: 'none',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'main.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      templateContent: template
-    }),
-    new ScriptExtHtmlWebpackPlugin({
-      inline: 'main',
-    })
-  ],
+	mode: 'none',
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, output),
+		filename: 'main.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			templateContent: template
+		}),
+		new ScriptExtHtmlWebpackPlugin({
+			inline: 'main',
+		})
+	],
 };
